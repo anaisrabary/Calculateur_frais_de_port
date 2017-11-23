07ca7957c48c4e1aa77c205d170a6480f166cfbe
anaisrabary <anais.rabary@gmail.com>
2017-11-23 18:06:36 +0100

    Frenc package Tested !

--- src/main/java/com/fredericboisguerin/insa/Package.java
+++ src/main/java/com/fredericboisguerin/insa/Package.java
@@ -6,6 +6,12 @@ import java.math.MathContext;
 import static java.math.RoundingMode.HALF_EVEN;
 
 public class Package {
+    public static final double COST_SMALL_PACKAGE = 12 ;
+    public static final double UNIT_COST_MIDIUM_WEIGHT = 17.59;
+    public static final double STATIC_FEE_MEDIUM_WEIGHT = 2.86;
+    public static final double UNIT_COST_BIG_WEIGHT =21.62;
+    public static final double UNIT_COST_BIG_VOLUM= 1.43;
+
     private int height ;
     private int width ;
     private int depth ;
@@ -23,14 +29,21 @@ public class Package {
 
         double result ;
         if ((height <= 229 && width <= 162) || (width <= 229 && height <= 162) && depth <= 25)
-            result = 12.00 ;
+           return COST_SMALL_PACKAGE;
         else
         {
             if (weight <= 1.8)
-                result= (weight*17.59)+2.86 ;
-            else result = ((weight*21.62) + (calculateVolum()*1.43 / Math.pow(10.0,6.0))) ;
+                return new BigDecimal(
+                        (weight*UNIT_COST_MIDIUM_WEIGHT)+ STATIC_FEE_MEDIUM_WEIGHT,
+                        new MathContext(4,HALF_EVEN))
+                        .doubleValue();
+            else
+                return new BigDecimal(
+                        Math.max((weight*UNIT_COST_BIG_WEIGHT) ,
+                                (calculateVolum()*UNIT_COST_BIG_VOLUM / Math.pow(10.0,6.0))),
+                        new MathContext(4,HALF_EVEN))
+                        .doubleValue();
         }
-        return new BigDecimal(result,new MathContext(4,HALF_EVEN)).doubleValue();
 
     }
 
